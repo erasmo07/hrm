b94ee8b94adbec8ac7bbf73015ae55d6d8eb040c
Erasmo Montes <erasmomontes07@gmail.com>
2019-10-17 16:31:21 -0400

    Change architecture project.

--- config/settings/base.py
+++ config/settings/base.py
@@ -150,7 +150,7 @@ STATICFILES_FINDERS = [
 # MEDIA
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#media-root
-MEDIA_ROOT = str(APPS_DIR("media"))
+MEDIA_ROOT = str(APPS_DIR("static/media"))
 # https://docs.djangoproject.com/en/dev/ref/settings/#media-url
 MEDIA_URL = "/media/"
 
